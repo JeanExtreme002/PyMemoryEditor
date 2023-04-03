04ec3a49ce77b6512a28079d148128c722788eb1
Jean Loui Bernard Silva de Jesus <45553309+JeanExtreme002@users.noreply.github.com>
2023-04-03 03:27:13 -0300

    Update __init__.py

--- PyMemoryEditor/__init__.py
+++ PyMemoryEditor/__init__.py
@@ -6,7 +6,7 @@ reading and writing values in the process memory.
 """
 
 __author__ = "Jean Loui Bernard Silva de Jesus"
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 
 from .process import Process
 from .win32.enum import ProcessOperations
