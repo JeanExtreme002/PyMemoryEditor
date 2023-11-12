5a0b4b6dfc3c2a6c4d084f260102ca0b34d3ffff
JeanExtreme002 <jeangamerextreme@gmail.com>
2023-11-12 14:47:27 -0300

    created non-implementeded method for LinuxProcess

--- PyMemoryEditor/linux/process.py
+++ PyMemoryEditor/linux/process.py
@@ -57,6 +57,27 @@ class LinuxProcess(AbstractProcess):
         """
         return read_process_memory(self.pid, address, pytype, bufflength)
 
+    def search_by_value(
+        self,
+        pytype: Type[T],
+        bufflength: int,
+        value: Union[bool, int, float, str, bytes],
+        scan_type,
+        *,
+        progress_information: Optional[bool] = False,
+    ) -> Generator[Union[int, Tuple[int, dict]], None, None]:
+        """
+        Search the whole memory space, accessible to the process,
+        for the provided value, returning the found addresses.
+
+        :param pytype: type of value to be queried (bool, int, float, str or bytes).
+        :param bufflength: value size in bytes (1, 2, 4, 8).
+        :param value: value to be queried (bool, int, float, str or bytes).
+        :param scan_type: the way to compare the values.
+        :param progress_information: if True, a dictionary with the progress information will be return.
+        """
+        raise NotImplementedError("Not implemented at this version.")
+
     def write_process_memory(
         self,
         address: int,
