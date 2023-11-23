1a1f5ad992fe790693d81fcf0b0a031e65a1c8e3
JeanExtreme002 <jeangamerextreme@gmail.com>
2023-11-23 13:31:05 -0300

    added --version arg

--- PyMemoryEditor/sample/application.py
+++ PyMemoryEditor/sample/application.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
+from PyMemoryEditor import __version__
 
 from .main_application_window import ApplicationWindow
 from .open_process_window import OpenProcessWindow
 
+import sys
+
 
 def main(*args, **kwargs):
+    if sys.argv[1].strip() in ["--version", "-v"]:
+        return print(__version__)
+
     open_process_window = OpenProcessWindow()
     process = open_process_window.get_process()
 
