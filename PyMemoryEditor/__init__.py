6853f6f1477218da8ed04dde45b19931760d863d
Jean Loui Bernard <45553309+JeanExtreme002@users.noreply.github.com>
2026-08-17 22:16:16 -0300

    chore: bump version to 2.1.0 (#81)

--- PyMemoryEditor/__init__.py
+++ PyMemoryEditor/__init__.py
@@ -8,7 +8,7 @@ Supported platforms: Windows, Linux and macOS (32-bit and 64-bit).
 """
 
 __author__ = "Jean Loui Bernard Silva de Jesus"
-__version__ = "2.0.1"
+__version__ = "2.1.0"
 
 import logging
 import sys
